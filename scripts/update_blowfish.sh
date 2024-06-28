667d334f2eb64590ace882e2a790623b31715b91
tjquillan <tjquillan@gmail.com>
2024-06-28 13:57:01 -0700

    Enhance update_blowfish script

--- scripts/update_blowfish.sh
+++ scripts/update_blowfish.sh
@@ -7,7 +7,11 @@ BLOWFISH_PATH="${BASE_DIR}/themes/blowfish"
 BLOWFISH_CONFIG_PATH="${BLOWFISH_PATH}/config.toml"
 GITHUB_PAGES_WORKFLOW_PATH="${BASE_DIR}/.github/workflows/pages.yml"
 
-BLOWFISH_VERSION="$1"
+if [ $# -eq 1 ]; then
+    BLOWFISH_VERSION="$1"
+else
+    BLOWFISH_VERSION="$(git -C $BLOWFISH_PATH describe --tags --abbrev=0)"
+fi
 
 git -C "${BLOWFISH_PATH}" fetch
 git -C "${BLOWFISH_PATH}" checkout "${BLOWFISH_VERSION}"
@@ -16,4 +20,6 @@ HUGO_VERSION="$(sed -nr "s/^max = \"(.*)\"$/\1/p" "${BLOWFISH_CONFIG_PATH}")"
 sed -r -i "s/(HUGO_VERSION:).*$/\1 ${HUGO_VERSION}/g" "${GITHUB_PAGES_WORKFLOW_PATH}"
 
 git -C "${BASE_DIR}" add "${BLOWFISH_PATH}" "${GITHUB_PAGES_WORKFLOW_PATH}"
-git -C "${BASE_DIR}" commit -m "Update Blowfish to ${BLOWFISH_VERSION}"
+if ! git diff --cached --quiet; then
+    git -C "${BASE_DIR}" commit -m "Update Blowfish to ${BLOWFISH_VERSION}"
+fi
