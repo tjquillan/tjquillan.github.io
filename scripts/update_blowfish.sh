debd3e8374208d2d357100bd1c89299e42be1d0e
tjquillan <tjquillan@gmail.com>
2024-08-11 11:17:44 -0700

    Fetch before getting version

--- scripts/update_blowfish.sh
+++ scripts/update_blowfish.sh
@@ -7,13 +7,14 @@ BLOWFISH_PATH="${BASE_DIR}/themes/blowfish"
 BLOWFISH_CONFIG_PATH="${BLOWFISH_PATH}/config.toml"
 GITHUB_PAGES_WORKFLOW_PATH="${BASE_DIR}/.github/workflows/pages.yml"
 
+git -C "${BLOWFISH_PATH}" fetch
+
 if [ $# -eq 1 ]; then
     BLOWFISH_VERSION="$1"
 else
     BLOWFISH_VERSION="$(git -C $BLOWFISH_PATH describe --tags --abbrev=0 origin/main)"
 fi
 
-git -C "${BLOWFISH_PATH}" fetch
 git -C "${BLOWFISH_PATH}" checkout "${BLOWFISH_VERSION}"
 
 HUGO_VERSION="$(sed -nr "s/^max = \"(.*)\"$/\1/p" "${BLOWFISH_CONFIG_PATH}")"
